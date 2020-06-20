6e5fc271989b59ae2c1c16aef520cf67f483026b
HZi Hey <nghiangominh99@gmail.com>
2020-06-20 23:19:16 +0700

    add all layout (panel & Frame)

--- src/src/main/java/com/hizinngo/hibernate/Main.java
+++ src/src/main/java/com/hizinngo/hibernate/Main.java
@@ -2,15 +2,15 @@ package com.hizinngo.hibernate;
 
 import com.hizinngo.hibernate.entity.Lop;
 import com.hizinngo.hibernate.dao.LopDAO;
+import com.hizinngo.hibernate.views.LoginView;
 
+import javax.swing.*;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        List<Lop> ds = LopDAO.layDanhSachLop();
-        System.out.println("So luong: " + ds.size());
-        for(Lop i : ds){
-            System.out.println(i.getMaLop());
-        }
+        JFrame a = new LoginView();
+
+        a.setVisible(true);
     }
 }
